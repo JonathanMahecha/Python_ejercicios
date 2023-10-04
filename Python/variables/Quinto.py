2ea4b17e1e6df0d39329cba1e155e82a84f82eab
JonathanMahecha <127152742+JonathanMahecha@users.noreply.github.com>
2023-10-04 14:38:28 -0500

    Variables

---
+++ Python/variables/Quinto.py
@@ -0,0 +1,5 @@
+#Programa para calcular el 20% de cualquier número de entrada.
+
+numero = int(input("Ingrese el numero que desea calcularle el 20%: "))
+resultado = ((20*numero)/100)
+print(f"El resultado del numero caculado al 20% es:  {resultado}")
