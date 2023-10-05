b894fe301180b50df74ee27f6a88ef78ce101318
JonathanMahecha <127152742+JonathanMahecha@users.noreply.github.com>
2023-10-04 19:39:51 -0500

    Delete Python/variables directory

--- Python/variables/Cuarto.py
+++
@@ -1,7 +0,0 @@
-#Programa que permita calcular la edad de una persona conociendo el año actual y el usuario digita su año de nacimiento.
-
-año_nacimiento = int(input("Ingrese el año de nacimiento: "))
-año_actual = int(input("Ingrese el año actual: "))
-edad = año_nacimiento-año_actual
-print(f"Usted tiene  {edad} de edad")
-
--- Python/variables/Primero.py
+++
@@ -1,8 +0,0 @@
-#Programa que permita multiplicar 3 números.
-
-print("Ingrese los siguientes datos")
-numero_1 = int(input('Ingrese el numero 1: '))
-numero_2 = int(input('Ingrese el numero 2: '))
-numero_3 = int(input('Ingrese el numero 3: '))
-multiplicacion = numero_1 * numero_2 * numero_3
-print(f"El resultado de la multiplicación es:  {multiplicacion}")
