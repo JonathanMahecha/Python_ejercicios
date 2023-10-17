58b4672305efb5111218de783e7cabd74064d1d0
JonathanMahecha <127152742+JonathanMahecha@users.noreply.github.com>
2023-10-17 14:34:14 -0500

    Delete Python_ejercicios-main/Python directory

--- Python_ejercicios-main/Python/Condicionales/Primero.py
+++
@@ -1,7 +0,0 @@
-#Solicitar número al usuario y determinar si es par, impar o es cero. 
-
-numero = int(input("Ingrese el numero el cual desea saber si es entero o impar: "))
-
-if numero == 0: print("El numero es igual a 0")
-elif numero % 2 == 0: print("El numero es par")
-else: print("El numero es impar")
--- Python_ejercicios-main/Python/Poo/Primero.py
+++
@@ -1,4 +0,0 @@
-#Escriba un programa para mostrar los primeros 10 números naturales.
-
-for i in range (1, 11):
-    print(f"{i}")
